c1921ca3098f1d893c4fa7635b7fd9fe7557e4f0
dlalloyd <116102568+dlalloyd@users.noreply.github.com>
2023-02-16 14:23:11 +0000

    reformated HTML

--- starter/js/logic.js
+++ starter/js/logic.js
@@ -1,23 +1,14 @@
-var startBtn = document.getElementById("startBtn");
-var submitBtn = document.querySelector("#submitBtn");
-var highScoreBtn = document.querySelector("#highScoreBtn");
+var currentTime = document.getElementById("timercountdown ");
+var timer = document.getElementById("startBtn");
+var questions = document.querySelector("start-screen");
+var wrapper = document.querySelector("#wrapper");
 
-// var clearButton = document.querySelector("#clearBtn");
-// var userInitialsSpan = document.querySelector("#userhighscores");
-// var userResultSpan = document.querySelector("#final-score");
-
-var timerEl = document.getElementById("timercountdown");
-var questions = document.querySelector("#questions");
-var choices = Array.from(document.querySelectorAll(".choice-text"));
+var score = 0;
+var questionIndex = 0;
 
 var remainingTime = 60;
 var scorePoints = 20;
 var qMax = 4;
-
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let availableQuestions = [];
 
 // Function that starts the timer.
 function countdown() {
@@ -37,58 +28,57 @@ function countdown() {
   }, 1000);
 }
 
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let availableQuestions = [];
+
 let quiz = [
   {
     question: "commonly used data types do not include:",
-    choice1: "strings",
-    choice2: "booleans",
-    choice3: "alerts",
-    choice4: "numbers",
-    answer: 3,
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
   },
   {
     question: "the condition in an if/else statement is enclosed within ___.",
-    choice1: "quotes",
-    choice2: "curly brackets",
-    choice3: "parentheses",
-    choice4: "square brackets",
-    answer: 3,
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
   },
   {
     question: "arrays in javascript can be used to store ___.",
-    choice1: "numbers and strings",
-    choice2: "other arrays",
-    choice3: "booleans",
-    choice4: "all of the above",
-    answer: 4,
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
   },
   {
     question:
       "string values must be enclosed within ___ when being assigned to variables.",
-    choice1: "commas",
-    choice2: "curly bracket",
-    choice3: "quotes",
-    choice4: "parentheses",
-    answer: 3,
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
   },
   {
     question:
       "a very useful tool used during development and debugging for printing current content to the debugger is:",
-    choice1: "javascript",
-    choice2: "terminal/bash",
-    choice3: "for loops",
-    choice4: "console.log",
-    answer: 4,
+    choices: ["javascript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
   },
 ];
+
 // Function that starts the quiz; timer starts, home screen hidden & question screen shown, score in local storage is reset.
-startButton.addEventListener("click", function () {
-  console.log("this works");
+startBtn.addEventListener("click", function () {
   localStorage.setItem("recentScore", score);
   hideHome();
   showAnswer();
   countdown();
 });
+
+console.log("js works");
+
+// Function for username display on highscores
 
 // This should generate questions and ddisplay them on the screen
 function newQuestion() {
@@ -105,5 +95,4 @@ function startQuiz() {
   availableQuestions = [...quiz];
   newQuestion();
 }
-
 startQuiz();
--- starter/js/scores.js
+++ starter/js/scores.js
@@ -1,5 +1,3 @@
 var clearButton = document.querySelector("#clearBtn");
 var userInitialsSpan = document.querySelector("#userhighscores");
 var userResultSpan = document.querySelector("#final-score");
-
-// var backButton = document.querySelector("#homeBtn");
