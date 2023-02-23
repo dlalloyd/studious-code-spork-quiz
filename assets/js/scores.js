35577c5cd7f98dde6459618e82ce74053668f259
dlalloyd <116102568+dlalloyd@users.noreply.github.com>
2023-02-23 10:40:46 +0000

    reformatted html file

---
+++ assets/js/scores.js
@@ -0,0 +1,13 @@
+function printHighScores() {
+  // To complete
+}
+
+function clearHighScores() {
+  // To complete
+}
+
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScores);
+
+//  document.getElementById('clear')
+printHighScores();
